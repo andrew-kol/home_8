0c49f2742181c9e389c659bccaab1b1606c685eb
andrew-kol <110204316+andrew-kol@users.noreply.github.com>
2022-08-25 23:33:26 +0300

    коммит2

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public static void main(String[] args){
+        doYear(2023);
+    }
 
 
-
+    public static void doYear(int year) {
+        System.out.println("\nЗадание 1.");
+        if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0))) System.out.println(year + " не високосный год");
+        else System.out.println(year + " високосный год");
+    }
 }
