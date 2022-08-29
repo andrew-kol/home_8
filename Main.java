f19b8125ccd8b84e6a6bbb4fb770c176a6bbbb3d
andrew-kol <110204316+andrew-kol@users.noreply.github.com>
2022-08-29 22:49:43 +0300

    коммит6

--- Main.java
+++ Main.java
@@ -1,40 +1,55 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         printYear(2023);
-        printPhoneInfo(2020);
-        calculateDeliveryDays(95);
+        printPhoneInfo(0, 2015);
+        //      calculateDeliveryDays(95);
     }
 
 
-    public static void printYear(int year) {
+    private static void printYear(int year) {
         System.out.println("\nЗадание 1.");
         if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0)))
             System.out.println(year + " не високосный год");
-        else System.out.println(year + " високосный год");
+        else {
+            System.out.println(year + " високосный год");
+        }
     }
 
-    public static void printPhoneInfo(int currentyear) {
+    private static void printPhoneInfo(int phoneOs, int productionYear) {
         System.out.println("\nЗадание 2.");
-        int clientOS = 1;
-        long clientDeviceYear = 2015L;
-        if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите обычную версию Android по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию Android по ссылке");
-        }
-    }
 
-    public static void calculateDeliveryDays(int deliveryDistance) {
-        System.out.println("\nЗадание 3.");
+        if (!(phoneOs == 0 || phoneOs == 1)) ;
+        {
+            throw new RuntimeException("Устройство поддерживается немедленно");
+        }
 
-        if (deliveryDistance <= 20) {
-            System.out.println("Доставка занимает сутки");
+            var currentYear = LocalDate.now().getYear();
+            String versionMessage = productionYear < currentYear ? "облегченную" : "";
+            String osMessage = phoneOs == 0 ? "iOs" : "Android";
+
+            System.out.printf("Установите %s версию приложения для %s по ссылке %s", versionMessage, osMessage);
         }
-        if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Доставка занимает двое суток");
-        }
-        if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Доставка занимает трое суток");
-        }
-    }
-}
+
+
+
+                    }
+
+
+                //  public static void calculateDeliveryDays(int deliveryDistance) {
+                ///    System.out.println("\nЗадание 3.");
+
+                // if (deliveryDistance <= 20) {
+                //   System.out.println("Доставка занимает сутки");
+                //}
+                //if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                //  System.out.println("Доставка занимает двое суток");
+                //}
+                //if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                //  System.out.println("Доставка занимает трое суток");
+                //}
+
+
+
+
